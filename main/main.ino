7fb8a3f2b0f80ca2bc73706de6015e0f8437b6b3
ndsantos <n.d.santos@gmail.com>
2023-04-24 16:48:27 +0100

    with wifi (not working as expected)

--- main/main.ino
+++ main/main.ino
@@ -1,189 +1,186 @@
 #include "network-config.h"
-#include <FastLED.h>
+#include "network-ip-address.h"
+#include "ESP8266WiFi.h"
+#include "ESPAsyncTCP.h"
+#include "ESPAsyncWebSrv.h"
+#include "FastLED.h"
+#define FASTLED_ALLOW_INTERRUPTS 0
+#define NUM_LEDS 74
+#define led_pin D7
+#define LED_TYPE WS2811
+CRGBArray<NUM_LEDS> leds;
+ 
+const char *ssid = wifi_ssa;
+const char *password = wifi_key;
+String theInput = "0";
+const char *PARAM_INPUT = "value";
 
-#define LED_PIN     D7
-#define NUM_LEDS    14
-#define BRIGHTNESS  64
-#define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
-CRGB leds[NUM_LEDS];
+const char index_html[] PROGMEM = R"=====(
+<!DOCTYPE html>
+  <html>
+    <head>
+    <meta charset="utf-8"/>
+      <style>
+        h1  {color: Grey; font-family: tahoma; font-size: 350%; text-align: center; margin-top: 100px;}
+        .center{display:grid; justify-content:center;}
+        [type="color"] {opacity: 40;width: 300px;height: 200px; border-radius:5px;}
+        .hex {font-family: tahoma; font-weight: bold; font-style:italic; text-align:center; justify-content: center; margin:25px;}
+        .buttoncontainer{display:grid; justify-content:center; grid-template-columns: 150px 150px; grid-gap:15px;}
+        button{outline:none; border:none; font-family: tahoma; font-weight:bold; font-size:20px; opacity:0.8; border-radius:10px; width:auto; padding:20px;}        
+        button:active{opacity:1.0;}
+        #red{background-color:Salmon;}
+        #green{background-color:#2bba18;}
+      </style>
+      <title>LED Sign Controller</title>
+      <meta name="viewport" content="width=device-width", intital-scale=1>
+    </head>
+    <body>
+      <h1>Select Color</h1>
+          <div class="center"><input type="color"  id="colorPicker" value="#cc0000" class="picker" >
+            <label class="hex" id="hex">Signature Red</label>
+          </div>
+          <div class="buttoncontainer">
+            <button type="submit" id="green">Grün</button>
+            <button type="submit" id="red">Lachsrot</button>
+          </div>
+      
+      <script>      
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/color?value=r204g0b0", true);
+        xhr.send();
 
-#define UPDATES_PER_SECOND 100
+        colorPicker.addEventListener("input", updateHex, false);
+        
+          function updateHex(event){
+          document.getElementById("hex").innerHTML = document.getElementById("colorPicker").value;
+          var xhr = new XMLHttpRequest();
+          var hexcode = document.getElementById("colorPicker").value;
+          var rgbcode = hexToRGB(hexcode.slice(1));
+          console.log(rgbcode);
+          var url = "/color?value=" + rgbcode;
+          xhr.open("GET", url, true);
+          xhr.send();
+          }
+          
+          function hexToRGB(hex){
+          var aRgbHex = hex.match(/.{1,2}/g);
+          var aRgb = [parseInt(aRgbHex[0], 16),
+                  parseInt(aRgbHex[1], 16),
+                  parseInt(aRgbHex[2], 16)];
+          var rgbcode = "r"+aRgb[0]+"g"+aRgb[1]+"b"+aRgb[2];
+          return rgbcode;
+          }
+          
+        document.getElementById("red").addEventListener("click", red, false);       
+          function red(event){
+            console.log("The red button has been clicked!");
+            document.getElementById("hex").innerHTML = "Lachsrot Teslas";
+            document.getElementById("colorPicker").value = "#fa8072";
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/color?value=r213g109b86", true);
+            xhr.send();
+          }
+          
+        document.getElementById("green").addEventListener("click", green, false);
+          function green(event){
+            console.log("The green button has been clicked!");
+            document.getElementById("hex").innerHTML = "Grüne Mitsubishis";
+            document.getElementById("colorPicker").value = "#2bba18";
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/color?value=r39g186b39", true);
+            xhr.send();
+          }
+      </script>
+    </body>
+  </html>
+)=====";
 
-// This example shows several ways to set up and use 'palettes' of colors
-// with FastLED.
-//
-// These compact palettes provide an easy way to re-colorize your
-// animation on the fly, quickly, easily, and with low overhead.
-//
-// USING palettes is MUCH simpler in practice than in theory, so first just
-// run this sketch, and watch the pretty lights as you then read through
-// the code.  Although this sketch has eight (or more) different color schemes,
-// the entire sketch compiles down to about 6.5K on AVR.
-//
-// FastLED provides a few pre-configured color palettes, and makes it
-// extremely easy to make up your own color schemes with palettes.
-//
-// Some notes on the more abstract 'theory and practice' of
-// FastLED compact palettes are at the bottom of this file.
+IPAddress local_ip(ip_address_1,ip_address_2,ip_address_3,ip_address_led_stripe);
+IPAddress gateway(ip_address_1,ip_address_2,ip_address_3,ip_address_gateway);
+IPAddress subnet(255,255,255,0);
+ 
+AsyncWebServer server(80);
 
+void setup(){
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  Serial.begin(9600);
+  delay(100);
+  WiFi.begin(ssid, password);
 
+  if (!WiFi.config(local_ip, gateway, subnet)) {
+    Serial.println("STA Failed to configure");
+  }
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(2000);
+    Serial.println("Connecting to Wifi..."); 
+  }
 
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
+  //NEOPIXEL
+  FastLED.addLeds<LED_TYPE,led_pin>(leds, NUM_LEDS);
 
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+  //WiFi.softAP(ssid, password);
+  //WiFi.mode(WIFI_AP);
+  //WiFi.softAPConfig(local_ip, gateway, subnet);
 
-
-void setup() {
-    delay( 3000 ); // power-up safety delay
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(  BRIGHTNESS );
-    
-    currentPalette = RainbowColors_p;
-    currentBlending = LINEARBLEND;
+  Serial.print("IP addresse: ");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.softAPIP());
+ 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+  
+  server.on("/color", HTTP_GET, [] (AsyncWebServerRequest * request){
+    String inputMessage;
+    if (request->hasParam(PARAM_INPUT)) {
+      inputMessage = request->getParam(PARAM_INPUT)->value();
+      convertHextoRGB(inputMessage);
+      Serial.println(inputMessage);
+      delay(5000);
+      convertHextoRGB(inputMessage);
+    }
+    else{
+      Serial.println("No new color set");
+    }
+    request->send(200, "text/plain", "OK");
+  });
+  server.onNotFound([](AsyncWebServerRequest *request){
+    request->send(404);
+  });
+ 
+  server.begin();
+}
+ 
+void loop(){
+  for(int i=0; i<NUM_LEDS; i++){
+    leds[i] = 0xFF44DD;
+   }
+  FastLED.show();
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
+void convertHextoRGB(String rgbcode){
+  int pos1 = rgbcode.indexOf('r');
+  int pos2 = rgbcode.indexOf('g');
+  int pos3 = rgbcode.indexOf('b');
 
-void loop()
-{
-    ChangePalettePeriodically();
-    
-    static uint8_t startIndex = 0;
-    startIndex = startIndex + 1; /* motion speed */
-    
-    FillLEDsFromPaletteColors( startIndex);
-    
+  int red = (rgbcode.substring(pos1+1,pos2)).toInt();
+  int green = (rgbcode.substring(pos2+1,pos3)).toInt();
+  int blue = (rgbcode.substring(pos3+1)).toInt();
+
+  for(int i=0; i<NUM_LEDS; i++){
+    leds[i].r = red;
+    leds[i].g = green;
+    leds[i].b = blue;
     FastLED.show();
-    FastLED.delay(1000 / UPDATES_PER_SECOND);
-}
-
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
-{
-    uint8_t brightness = 255;
-    
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
-    }
-}
-
-
-// There are several different palettes of colors demonstrated here.
-//
-// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
-// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
-//
-// Additionally, you can manually define your own color palettes, or you can write
-// code that creates color palettes on the fly.  All are shown here.
-
-void ChangePalettePeriodically()
-{
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
-    
-    if( lastSecond != secondHand) {
-        lastSecond = secondHand;
-        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
-        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
-        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
-        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
-        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
-        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
-        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
-        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
-        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
-    }
-}
-
-// This function fills the palette with totally random colors.
-void SetupTotallyRandomPalette()
-{
-    for( int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV( random8(), 255, random8());
-    }
-}
-
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-void SetupBlackAndWhiteStripedPalette()
-{
-    // 'black out' all 16 palette entries...
-    fill_solid( currentPalette, 16, CRGB::Black);
-    // and set every fourth one to white.
-    currentPalette[0] = CRGB::White;
-    currentPalette[4] = CRGB::White;
-    currentPalette[8] = CRGB::White;
-    currentPalette[12] = CRGB::White;
-    
-}
-
-// This function sets up a palette of purple and green stripes.
-void SetupPurpleAndGreenPalette()
-{
-    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-    CRGB green  = CHSV( HUE_GREEN, 255, 255);
-    CRGB black  = CRGB::Black;
-    
-    currentPalette = CRGBPalette16(
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black,
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black );
-}
-
-
-// This example shows how to set up a static color palette
-// which is stored in PROGMEM (flash), which is almost always more
-// plentiful than RAM.  A static PROGMEM palette like this
-// takes up 64 bytes of flash.
-const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
-{
-    CRGB::Red,
-    CRGB::Gray, // 'white' is too bright compared to red and blue
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Blue,
-    CRGB::Black,
-    CRGB::Black
-};
-
-
-
-// Additionl notes on FastLED compact palettes:
-//
-// Normally, in computer graphics, the palette (or "color lookup table")
-// has 256 entries, each containing a specific 24-bit RGB color.  You can then
-// index into the color palette using a simple 8-bit (one byte) value.
-// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
-// is quite possibly "too many" bytes.
-//
-// FastLED does offer traditional 256-element palettes, for setups that
-// can afford the 768-byte cost in RAM.
-//
-// However, FastLED also offers a compact alternative.  FastLED offers
-// palettes that store 16 distinct entries, but can be accessed AS IF
-// they actually have 256 entries; this is accomplished by interpolating
-// between the 16 explicit entries to create fifteen intermediate palette
-// entries between each pair.
-//
-// So for example, if you set the first two explicit entries of a compact 
-// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
-// the first sixteen entries from the virtual palette (of 256), you'd get
-// Green, followed by a smooth gradient from green-to-blue, and then Blue.
+  }
+  Serial.print("Red:");
+  Serial.println(red);
+  Serial.print("Green:");
+  Serial.println(green);
+  Serial.print("Blue:");
+  Serial.println(blue);
+  }
